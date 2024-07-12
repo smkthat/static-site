72ebcf31d07247edbcf1231cdaeeb6f5059bae82
Mikhail Vlasenko <ya@smkthat.ru>
2024-07-12 23:43:52 +1100

    refactoring, sub navigation menu, js example modules added

---
+++ assets/js/modules/greet.js
@@ -0,0 +1,10 @@
+export default function greet(name) {
+  /**
+   * A function that displays an alert with a greeting message.
+   *
+   * @param {string} name - The name to greet.
+   * @return {void} No return value.
+   */
+
+  alert(`Hello, ${name}!`);
+}
